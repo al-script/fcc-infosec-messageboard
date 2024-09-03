bcccd20b09ee7bc3a184cd464811ae12a8d9810a
al-script <alec.sorensen.as@gmail.com>
2024-09-03 14:01:41 -0700

    Change status reply message for deleting reply

--- routes/api.js
+++ routes/api.js
@@ -844,11 +844,13 @@ module.exports = function (app) {
 
         bcrypt.compare(delete_password, reply_hash, function (err, result) {
           if (!result) {
-            res.status(200).json({ message: "incorrect password" });
+            // res.status(200).json({ message: "incorrect password" });
+            res.status(200).json("incorrect password" );
             console.log(requestLogPrefix, "Failure, incorrect password");
           } else {
             handleDeleteReply(board_id, thread_id, reply_id, requestLogPrefix);
-            res.status(200).json({ message: "success" });
+            // res.status(200).json({ message: "success" });
+            res.status(200).json("success");
             console.log(requestLogPrefix, "Success");
           }
           return;
